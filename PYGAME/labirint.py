68af48da4b9a0d4c03ec49e60ff32fa8d7508024
KenChangJo <kenchang986754@gmail.com>
2025-11-28 22:49:54 +0700

    Add files via upload

---
+++ PYGAME/labirint.py
@@ -0,0 +1,157 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_x_speed, player_y_speed):
+        super().__init__(player_image, player_x, player_y, size_x, size_y)
+        self.x_speed = player_x_speed
+        self.y_speed = player_y_speed
+
+    def update(self):
+        # Horizontal movement
+        if 0 <= self.rect.x + self.x_speed <= win_width - 80:
+            self.rect.x += self.x_speed
+
+        platforms_touched = sprite.spritecollide(self, barrier, False)
+        if self.x_speed > 0:
+            for p in platforms_touched:
+                self.rect.right = min(self.rect.right, p.rect.left)
+        elif self.x_speed < 0:
+            for p in platforms_touched:
+                self.rect.left = max(self.rect.left, p.rect.right)
+
+        # Vertical movement
+        if 0 <= self.rect.y + self.y_speed <= win_height - 80:
+            self.rect.y += self.y_speed
+
+        platforms_touched = sprite.spritecollide(self, barrier, False)
+        if self.y_speed > 0:
+            for p in platforms_touched:
+                self.rect.bottom = min(self.rect.bottom, p.rect.top)
+        elif self.y_speed < 0:
+            for p in platforms_touched:
+                self.rect.top = max(self.rect.top, p.rect.bottom)
+
+    def fire(self):
+        bullet = Bullet('bullet.png', self.rect.right, self.rect.centery, 15, 20, 15)
+        bullets.add(bullet)
+
+
+class Enemy(GameSprite):
+    side = "left"
+
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__(player_image, player_x, player_y, size_x, size_y)
+        self.speed = player_speed
+
+    def update(self):
+        if self.rect.x <= 420:
+            self.side = "right"
+        if self.rect.x >= win_width - 85:
+            self.side = "left"
+        if self.side == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
+
+class Bullet(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__(player_image, player_x, player_y, size_x, size_y)
+        self.speed = player_speed
+
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x > win_width + 10:
+            self.kill()
+
+
+# Creating a window
+win_width = 1000
+win_height = 500
+display.set_caption("Maze")
+window = display.set_mode((win_width, win_height))
+back = (119, 210, 223)
+
+# Creating objects
+w1 = GameSprite('platform2.png', win_width / 2 - win_width / 3, win_height / 2, 300, 50)
+w2 = GameSprite('platform2_v.png', 370, 100, 50, 400)
+barrier = sprite.Group()
+barrier.add(w1)
+barrier.add(w2)
+
+packman = Player("hero.png", 5, win_height - 80, 80, 80, 0, 0)
+final_sprite = GameSprite('pac-1.png', win_width - 85, win_height - 100, 80, 80)
+monster = Enemy('cyborg.png', win_width - 80, 180, 80, 80, 5)
+monster2 = Enemy('cyborg.png', win_width - 120, 230, 80, 80, 5)
+monsters = sprite.Group(monster, monster2)
+bullets = sprite.Group()
+
+# Game loop
+run = True
+finish = False
+clock = time.Clock()
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_LEFT:
+                packman.x_speed = -5
+            elif e.key == K_RIGHT:
+                packman.x_speed = 5
+            elif e.key == K_UP:
+                packman.y_speed = -5
+            elif e.key == K_DOWN:
+                packman.y_speed = 5
+            elif e.key == K_SPACE:
+                packman.fire()
+        elif e.type == KEYUP:
+            if e.key in [K_LEFT, K_RIGHT]:
+                packman.x_speed = 0
+            elif e.key in [K_UP, K_DOWN]:
+                packman.y_speed = 0
+
+    if not finish:
+        window.fill(back)
+        packman.update()
+        bullets.update()
+        monsters.update()
+
+        barrier.draw(window)
+        bullets.draw(window)
+        monsters.draw(window)
+        final_sprite.reset()
+        packman.reset()
+
+        # Collisions
+        sprite.groupcollide(monsters, bullets, True, True)
+        sprite.groupcollide(bullets, barrier, True, False)
+
+        if sprite.spritecollide(packman, monsters, False):
+            finish = True
+            img = image.load('game-over_1.png')
+            window.fill((255, 255, 255))
+            d = img.get_width() // img.get_height()
+            window.blit(transform.scale(img, (win_height * d, win_height)), (90, 0))
+
+        if sprite.collide_rect(packman, final_sprite):
+            finish = True
+            img = image.load('thumb.jpg')
+            window.fill((255, 255, 255))
+            window.blit(transform.scale(img, (win_width, win_height)), (0, 0))
+
+    display.update()
+    clock.tick(60)
